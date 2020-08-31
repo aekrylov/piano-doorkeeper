1ddd998193b4f0d6b5ae33eeb937fa40e62ccde1
Anton Krylov <anthony.kryloff@gmail.com>
2020-08-31 22:58:46 +0300

    add some missing unit tests

---
+++ src/test/kotlin/me/aekrylov/piano_doorkeeper/AccessControlServiceTest.kt
@@ -0,0 +1,40 @@
+package me.aekrylov.piano_doorkeeper
+
+import io.kotlintest.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class AccessControlServiceTest {
+
+    private val service = AccessControlService()
+
+    @ParameterizedTest
+    @MethodSource("positives")
+    fun `positive cases`(roomId: Int, user: User) {
+        service.hasAccess(roomId, user) shouldBe true
+    }
+
+    @ParameterizedTest
+    @MethodSource("negatives")
+    fun `negative cases`(roomId: Int, user: User) {
+        service.hasAccess(roomId, user) shouldBe false
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun positives() = listOf(
+                Arguments.of(1, User(1)),
+                Arguments.of(1, User(3)),
+                Arguments.of(14, User(42))
+        )
+
+        @JvmStatic
+        fun negatives() = listOf(
+                Arguments.of(2, User(1)),
+                Arguments.of(2, User(3)),
+                Arguments.of(14, User(15))
+        )
+    }
+}
